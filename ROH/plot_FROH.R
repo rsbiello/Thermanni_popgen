7764cd9e8b12b2a1e0411f3a4a9980508e4eab1a
rsbiello <darwin23@hotmail.it>
2024-11-22 22:10:10 +0100

    Update plot_FROH.R

--- ROH/plot_FROH.R
+++ ROH/plot_FROH.R
@@ -1,1 +1,15 @@
+library(ggplot2)
+library(tidyverse)
 
+##load table
+data <- read.table("FROH.txt", header=T)
+data
+
+plot<-ggplot(data,aes(ind, ROH, fill=pop)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  theme_classic() +
+  xlab("Pops") +
+  ylab("FROH") +
+  coord_flip(ylim=c(0,100))
+
+plot
