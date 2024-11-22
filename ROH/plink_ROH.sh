86292494f028320ce7f14b74845d06e7aa279e21
rsbiello <33647427+rsbiello@users.noreply.github.com>
2024-11-22 22:24:52 +0100

    Update plink_ROH.sh

--- ROH/plink_ROH.sh
+++ ROH/plink_ROH.sh
@@ -2,6 +2,6 @@
 
 module load plink-1.90
 
-VCF=Thermanni.callable.89scaffs.vcf.gz
+VCF= #/path/to/VCF/xxx.vcf.gz
 
-plink --vcf $VCF --keep-allele-order --allow-extra-chr --homozyg-window-snp 50 --homozyg-kb 50 --homozyg-snp 100 --homozyg-density 50 --homozyg-gap 1000 --homozyg-window-het 0 --homozyg-window-missing 5 --homozyg-window-threshold 0.05 --het --out roh.89scaffs.custom.hom_50kb.het_0
+plink --vcf $VCF --keep-allele-order --allow-extra-chr --homozyg-window-snp 50 --homozyg-kb 50 --homozyg-snp 100 --homozyg-density 50 --homozyg-gap 1000 --homozyg-window-het 0 --homozyg-window-missing 5 --homozyg-window-threshold 0.05 --het --out roh.custom.hom_50kb.het_0
