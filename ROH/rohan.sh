381a1fcc3f22c36f7b8b38d9279bf9d79f1f0d6c
rsbiello <33647427+rsbiello@users.noreply.github.com>
2024-11-22 23:00:58 +0100

    Update rohan.sh

--- ROH/rohan.sh
+++ ROH/rohan.sh
@@ -2,9 +2,9 @@
 
 module load rohan-1.0
 
-REF=rTesHer1.1.fasta
-CHR=list_89scaffs_1Mb_mod
-BAM=PNV_9_HB_S24_001_sorted_dedup_rg_real_rmopt.bam
-BED=Thermanni.callable.bed
+REF= #/path/to/reference/xxx.fa
+CHR= #/path/to/list/chromosomes
+BAM= #/path/to/BAM/xxx.bam
+BED= #/path/to/map/xxx.bed
 
 rohan -t 16 -o PNV_9 --rohmu 5e-05 --map $BED --auto $CHR --tstv 1.85 --size 100000 $REF $BAM
